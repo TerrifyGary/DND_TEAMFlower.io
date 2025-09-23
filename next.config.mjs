fdea4534fb22a74d51162cf08f0566a68f0c276b
TerrifyGary <yoyoman1415926@gmail.com>
2025-09-23 15:46:44 +0800

    docs(next): Refine basePath comment in Next.js config
    Updated the inline comment for the `basePath` configuration in `next.config.mjs`.
    A trailing slash was added to the "GitHub Pages" description for minor consistency.

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  basePath: '/DND_TEAMFlower.io', // Set the base path for GitHub Pages
+  basePath: '/DND_TEAMFlower.io', // Set the base path for GitHub Pages/
 }
 
 export default nextConfig
